001e192114d85182c458a5852cf0eb871854feaf
Dykam <kramieb@gmail.com>
2014-01-08 15:29:37 +0100

    Return Empty array when annotations are missing

--- src/main/java/nl/dykam/dev/reutil/data/ComponentInfo.java
+++ src/main/java/nl/dykam/dev/reutil/data/ComponentInfo.java
@@ -5,7 +5,14 @@ import nl.dykam.dev.reutil.data.annotations.Defaults;
 import nl.dykam.dev.reutil.data.annotations.ObjectType;
 import nl.dykam.dev.reutil.data.annotations.Require;
 
+import java.lang.reflect.Array;
+
 class ComponentInfo {
+
+    public static final ObjectType[] OBJECT_TYPES = new ObjectType[0];
+    @SuppressWarnings("unchecked")
+    public static final Class<? extends Component>[] CLASSES = (Class<? extends Component>[]) Array.newInstance(Class.class, 0);
+
     public static <T extends Component> Defaults getDefaults(Class<T> type) {
         return type.getAnnotation(Defaults.class);
     }
@@ -15,9 +22,13 @@ class ComponentInfo {
     }
 
     public static <T extends Component> ObjectType[] getApplicables(Class<T> type) {
-        return type.getAnnotation(ApplicableTo.class).value();
+        ApplicableTo annotation = type.getAnnotation(ApplicableTo.class);
+        return annotation == null ? OBJECT_TYPES : annotation.value();
     }
+
+    @SuppressWarnings("unchecked")
     public static <T extends Component> Class<? extends Component>[] getRequired(Class<T> type) {
-        return type.getAnnotation(Require.class).value();
+        Require annotation = type.getAnnotation(Require.class);
+        return annotation == null ? CLASSES : annotation.value();
     }
 }
